eee21a96759ecbab943e050574d8f1a73cb22298
Bipin M <bipin.mandoti@succevo.com>
2021-04-27 20:47:40 +0530

    test

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -9,6 +9,11 @@ use Illuminate\Routing\Controller as BaseController;
 class MenuController extends BaseController
 {
     public function getMenuItems() {
-        throw new \Exception('implement in coding task 3');
+        //throw new \Exception('implement in coding task 3');
+
+        $menus = MenuItem::whereNull('parent_id')
+        ->with('childrenmenus')
+        ->get();
+        return response()->json($menus, 200);
     }
 }
--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -9,4 +9,12 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItem extends Model
 {
 
+    public function menus(){
+        return $this->hasMany(MenuItem::class,'parent_id','id');
+    }
+
+    public function childrenmenus(){
+        return $this->hasMany(MenuItem::class,'parent_id')->with('menus');
+    }
+
 }
